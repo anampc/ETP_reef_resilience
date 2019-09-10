e5f4aedd2eed6e5ef5533494bce03fc7ca50f289
Ana Palacio <ana.palacioc@gmail.com>
2019-09-10 15:35:17 -0400

    First commit

---
+++ DHW/1.Get_OISST_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Obtain SST data from NOAA OI SST V2 High Resolution Dataset
+# Needs ~18G of free space 
+
+# Go to the script directory
+	cd "${0%/*}"
+
+# Make a folder for the .nc files and navigate to it
+	mkdir Daily_OISST_v2
+	cd Daily_OISST_v2/
+
+
+# Get the SST data from the 1981-2014 period from the NOAA dataset
+# ftp://ftp.cdc.noaa.gov/Datasets/noaa.oisst.v2.highres/
+
+#wget ftp://ftp.cdc.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.* ./
+wget "ftp://ftp.cdc.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.*[1981-2014]*.nc" ./
